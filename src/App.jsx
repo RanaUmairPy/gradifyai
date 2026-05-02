830ff52f7d7621861c4cc1f7e8d8ecbe3899252c
RanaUmairPy <umairsaeed320@gmail.com>
2026-05-02 13:40:19 +0500

    Design improve

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Import Layout/Wrapper Component
+// Import Layout/Wrapper Components
 import Base from "./pages/Base";
 
 // Import Auth Pages
